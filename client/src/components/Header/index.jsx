815649790c089aefc2b03ca3978c99cd49866ba2
unknown <samtisme@gmail.com>
2023-09-12 16:12:14 -0700

    added styling to header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -20,34 +20,8 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import LogoutButton from '../LogoutButton'
-
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
+import LogoutButton from '../LogoutButton';
+import '../../pages/css/home.css';
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
@@ -150,74 +124,53 @@ const NavBar = () => {
   );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
+    <header className="app-bar">
+      <nav className="toolbar">
+        <div className="menu-icon">
+          <button
+            className="icon-button"
             aria-label="open drawer"
-            sx={{ mr: 2 }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+            ☰
+          </button>
+        </div>
+        <h1 className='siteName'>LoudMouth</h1>
+        <div className="actions">
+          {Auth.loggedIn() ? (
+            <LogoutButton />
+          ) : (
+            <a href="/login" className="login-button">
+              Log in
+            </a>
+          )}
+          <button
+            className="icon-button mail-button"
+            aria-label="show 4 new mails"
           >
-            LoudMouth
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            {Auth.loggedIn() ? (
-              <LogoutButton />
-              ) : (
-              <Button variant="outlined" color="inherit" size="small" href="/login">Log in</Button>)}
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                <MailIcon />
-            </IconButton>
-            
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </Box>
+            📬
+          </button>
+          <button
+            className="icon-button profile-button"
+            aria-label="account of current user"
+            onClick={handleProfileMenuOpen}
+          >
+            <span role="img" aria-label="user icon">
+              👤
+            </span>
+          </button>
+        </div>
+      </nav>
+      <div className="mobile-menu-icon">
+        <button
+          className="icon-button"
+          aria-label="show more"
+          onClick={handleMobileMenuOpen}
+        >
+          ☰
+        </button>
+      </div>
+    </header>
   );
+  
 }
 export default NavBar;
